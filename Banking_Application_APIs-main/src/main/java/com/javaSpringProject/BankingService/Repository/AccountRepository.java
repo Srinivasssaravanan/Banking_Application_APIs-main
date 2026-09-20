4bad7f05062c253c5519b5b9589f5d4b049da15a
Srinivasssaravanan <137177741+Srinivasssaravanan@users.noreply.github.com>
2026-09-20 12:45:32 +0530

    updated
    Removed unused imports from AccountRepository.

--- Banking_Application_APIs-main/src/main/java/com/javaSpringProject/BankingService/Repository/AccountRepository.java
+++ Banking_Application_APIs-main/src/main/java/com/javaSpringProject/BankingService/Repository/AccountRepository.java
@@ -1,13 +1,9 @@
 package com.javaSpringProject.BankingService.Repository;
 
 import com.javaSpringProject.BankingService.Entity.Account;
-import jakarta.persistence.LockModeType;
-import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.QueryHints;
-import org.springframework.data.repository.query.Param;
+
+
 
 import java.util.Optional;
 
